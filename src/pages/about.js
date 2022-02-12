cc58162d0704fcf8221e97d8b0d662671ef1513a
birnen9 <Birnen@yeah.net>
2022-02-12 18:21:32 +0800

    乞丐版页面

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>hello 与灰同尘</h1>
+      <p>欢迎来到我的blog</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
